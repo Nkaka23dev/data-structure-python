10f6af51bfd7f99e2753dca12e50f40389faf2fc
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-03 07:39:44 +0200

    merging files

---
+++ merge_file.py
@@ -0,0 +1,8 @@
+with open("files/nkaka2.txt","r") as cont:
+    read=cont.read()
+    with open("files/names.txt","r") as names:
+        for name in names:
+            message="Hello "+name.strip()+" consider the following.\n"+read
+            with open('files'+'/'+name.strip()+'.txt','w') as new_f:
+                new_f.write(message)
+            
