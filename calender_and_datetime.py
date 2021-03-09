1f973f2817cf34aa84527267291f5884cd83d127
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-09 07:23:37 +0200

    two functins added

---
+++ calender_and_datetime.py
@@ -0,0 +1,13 @@
+# import calendar
+
+# yy=2020
+# mm=7
+
+# print(calendar.month(yy,mm))
+
+import datetime
+
+# nkaka=datetime.date(2000,1,1)
+# print(nkaka.strftime("%A,%B %d,%Y"))
+
+print(datetime.datetime.today())
---
+++ mult_table.py
@@ -0,0 +1,6 @@
+def mult_table(numb):
+    for i in range(1,11):
+        print(f"{number}*{i}={numb*i}")
+
+number=int(input("Enter a number: "))
+mult_table(number)
