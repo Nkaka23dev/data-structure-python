fabf1d9a8e167865925a8231d1509ac531762767
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-16 09:17:07 +0200

    end of list exercices

--- list2.py
+++ list2.py
@@ -35,6 +35,43 @@ exp=[2200,2350,2600,2130,2190]
 # print(exp)
   
 
+heros=['spider man','thor','hulk','iron man','captain america']
+# # 1. Length of the list
+# print(len(heros))
+
+# #2. Add 'black panther' at the end of this list
+# heros.append("black panther")
+# print(heros)
+
+#3. You realize that you need to add 'black panther' after 'hulk',
+#    so remove it from the list first and then add it after 'hulk'
+# heros.remove(heros[-1])
+# heros.remove("black panther")
+# print(heros)
+
+# # 4. Now you don't like thor and hulk because they get angry easily :)
+# #    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+# #    Do that with one line of code.
+
+# heros[1:3]=["Doctor strange"]
+# print(heros)
+
+# #Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
+# heros.sort(reverse=True)
+# print(heros)
+
+# 3 Create a list of all odd numbers between 1 and a max number. Max number is something you need to take from a user using input() function
+
+
+# Create a list of all odd numbers between 1 and a max number. Max number is something you need to take from a user using input() function
+
+numbers=int(input("Enter a maximum number: "))
+print([i for i in list(range(1,numbers+1)) if i%2!=0])
+
+# print(list(map(lambda x:x**2,list(range(1,numbers+1)))))
+
+
+
 
    
     
