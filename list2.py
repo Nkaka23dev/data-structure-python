c938db5494a04aae98b258c188885baa776098fe
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-16 05:36:17 +0200

    exercise of data structure

---
+++ list2.py
@@ -0,0 +1,40 @@
+# exercices from data structure and algorithm from codebasics on youtube
+
+exp=[2200,2350,2600,2130,2190]
+#1
+# extra_spent=exp[1]-exp[0]
+# print(extra_spent)
+
+# #2
+# # total in first quarter
+# # total=0
+# # for i in exp[:3]:
+# #     total+=i
+# tot=exp[0]+exp[1]+exp[2]
+# print(tot)
+
+# # 3
+# # if we have spent exactly 2000 in any month
+
+# # for i in exp:
+# #     if i==2000:
+# #         print(True)
+# print(2000 in exp)
+
+#June month just finished and your expense is 1980 dollar.
+# Add this item to our monthly expense list
+
+# exp.append(1980)
+# print(exp)
+
+#  You returned an item that you bought in a month of April and
+# got a refund of 200$. Make a correction to your monthly expense list
+# based on th
+
+# exp[3]=exp[3]-200
+# print(exp)
+  
+
+
+   
+    
