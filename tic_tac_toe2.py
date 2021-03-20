f0021f9a8f689cb4f0d8604e14611d89fd1f5b31
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-20 11:36:08 +0200

    all about tic_tac_toe

--- tic_tac_toe2.py
+++ tic_tac_toe2.py
@@ -1,16 +1,231 @@
-game=[[1,0,4],
-      [0,6,4],
-      [1,3,4]]
+game=[[1,3,1],
+      [3,1,4],
+      [3,2,1]]
 
-# Verical winner
+# horizontal winner
+for row in game:
+    if row.count(row[0])==len(row):
+        print("horizontal winner!")
+
+#vertical winner
 for col in range(len(game)):
     check=[]
     for row in game:
-        check.append(row[col])
-    
-    if check.count(check[0])==len(check):
-        print("Winner!")
+        check.append(row[0])
 
+    if check.count(check[0])==len(check):
+        print("Vertical winner")
+
+#diagonals winner
+diags=[]
+for i,row in enumerate(range(len(game))):
+    diags.append(game[row][i])
+
+if diags.count(diags[0])==len(diags): 
+    print("Horizontal 1 winner.")
+
+diag=[]
+for i,row in enumerate(reversed(range(len(game)))):
+    diag.append(game[row][i])
+   
+if diag.count(diag[0])==len(diag):
+    print("Horizontal 2 winner")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# diags=[]
+# for col,row in enumerate(reversed(range(len(game)))):
+#     diags.append(game[row][col])
+
+# print(diags)
+
+# diags=[]
+# for ix in range(len(game)):
+#     diags.append(game[ix][ix])
+
+# print(diags)
+
+# cols=list(reversed(range(len(game))))
+# rows=range(len(game))
+
+# for idx in rows:
+#     print(idx,cols[idx])   
+
+# diag=[]
+# for ix in reversed(range(len(game))):
+#     diag.append(game[ix][ix])
+
+# print(diag)
+
+
+# diags=[]
+# for ix in range(len(game)):
+#     diags.append(game[ix][ix])
+
+# print(diags)
+
+# # diagonal winner hard coded
+# if game[0][0]==game[1][1]==game[2][2]:
+#     print("Winner")
+
+# if game[2][0]==game[1][1]==game[0][2]:
+#     print("Winner again!")
+
+# # Verical winner
+# for col in range(len(game)):
+#     check=[]
+#     for row in game:
+#         check.append(row[col])
+    
+#     print(check,end='')
+#     if check.count(check[0])==len(check):
+#         print("Winner!")
 
 # #Horizontal winner
 # for row in game:
